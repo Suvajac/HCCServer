79fd338ff620d0a797280e797c70938223f958a0
mils <mils@email.com>
2017-09-11 12:53:30 +0200

    evo

--- src/net/etfbl/hcc/Main.java
+++ src/net/etfbl/hcc/Main.java
@@ -4,19 +4,20 @@ import java.io.IOException;
 import java.net.*;
 
 import net.etfbl.hcc.util.ConnectionProperty;
+import net.etfbl.hcc.util.HCCUtil;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ServerSocket ss = null;
 		try {
-			ss = new ServerSocket(ConnectionProperty.getInstance().getServerTCPPort());
+			ss = new ServerSocket(ConnectionProperty.getInstance().getServerTCPPort()+1);
 			System.out.println("Running...");
 			while(true){
 				Socket sock = ss.accept();
 				new ServerThread(sock).start();
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/net/etfbl/hcc/ServerThread.java
+++ src/net/etfbl/hcc/ServerThread.java
@@ -1,17 +1,21 @@
 package net.etfbl.hcc;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
+import net.etfbl.hcc.model.Korisnik;
+import net.etfbl.hcc.util.HCCUtil;
 import net.etfbl.hcc.util.ProtokolPoruka;
 
 public class ServerThread extends Thread{
 	private Socket sock;
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
-	
+
 	public ServerThread(Socket sock){
 		try{
 			this.sock = sock;
@@ -22,36 +26,51 @@ public class ServerThread extends Thread{
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run(){
 		ProtokolPoruka ppin = null;
 		ProtokolPoruka ppout = null;
-		while(ppin==null || !ppin.getTip().equals("quit")){
-			try {
-				ppin = (ProtokolPoruka) in.readObject();
-				switch(ppin.getTip()){
-					case "radili" : 
-//						HCCUtil.getDAOFactory().getRacunDAO().getRacun(ppin.getListaObjekata().get(0));
-						break;
-					case "asdfg" :
-						break;
-					default :
-						ppout = new ProtokolPoruka("greska");
-				}
-				out.writeObject(ppout);
-			} 
-			catch (ClassNotFoundException | IOException e) {
-				e.printStackTrace();
-			}		
-		}
-
 		try {
-			in.close();
-			out.close();
-			sock.close();
-		} catch (IOException e) {
+			while(ppin==null || !ppin.getTip().equals("Korisnik.logout")){
+
+					ppin = (ProtokolPoruka) in.readObject();
+					switch(ppin.getTip()){
+						case "Korisnik.getKorisnik" :
+							System.out.println("Korisnik.getKorisnik");
+							Korisnik k=null;
+							k=HCCUtil.getDAOFactory().getGostDAO().getKorisnik((String)ppin.getListaObjekata().get(0));
+							if(k==null)
+								k=HCCUtil.getDAOFactory().getRecepcionarDAO().getKorisnik((String)ppin.getListaObjekata().get(0));
+							ArrayList<Object> rez=new ArrayList<Object>();
+							rez.add(k);
+							ppout=new ProtokolPoruka("response",rez);
+							break;
+						case "asdfg" :
+							break;
+						default :
+							ppout = new ProtokolPoruka("greska");
+					}
+					out.writeObject(ppout);
+
+			}
+		}catch (EOFException e) {
+			// TODO: handle exception al nikad
+			System.out.println("PUKOOOO");
+		}
+		catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
+		}
+		finally {
+			try {
+				in.close();
+				out.close();
+				sock.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Log out");
 		}
 	}
 }
