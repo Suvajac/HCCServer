2884e0e8a316f24865564d2bc43e2a6a08628c28
mils <mils@email.com>
2017-09-15 14:01:01 +0200

    radi jar

--- src/net/etfbl/hcc/util/ConnectionProperty.java
+++ src/net/etfbl/hcc/util/ConnectionProperty.java
@@ -1,8 +1,7 @@
 package net.etfbl.hcc.util;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 public class ConnectionProperty {
     private static ConnectionProperty instance;
@@ -31,70 +30,11 @@ public class ConnectionProperty {
         return Integer.parseInt(readValue("MulticastPort"));
     }
 
-    public void setServerIpAddress(String value){
-    	writeValue("ServerIpAdress", value);
-    }
-    public void setServerTCPPort(String value){
-    	writeValue("ServerTCPPort", value);
-    }
-    public void setMulticastIpAdress(String value){
-    	writeValue("MulticastIpAdress", value);
-    }
-    public void setMulticastPort(String value){
-    	writeValue("MulticastPort", value);
-    }
-
     private String readValue(String key){
-    	BufferedReader reader=null;
-        try {
-            reader = new BufferedReader(new FileReader("src/net/etfbl/hcc/util/ConnectionProperties.txt"));
-            String line = null;
-            while((line=reader.readLine())!=null){
-                if(line.startsWith(key)){
-                    return line.replaceAll(key+"=","");
-                }
-            }
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }finally{
-        	try {
-				reader.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-        return null;
+    	ResourceBundle bundle = PropertyResourceBundle
+                .getBundle("net.etfbl.hcc.util.ConnectionProperties");
+    	return bundle.getString(key);
     }
 
-    private void writeValue(String key,String value){
-    	try{
-            BufferedReader reader = new BufferedReader(new FileReader("src/net/etfbl/hcc/util/ConnectionProperties.txt"));
-            String l = null;
-            List<String> lines = new ArrayList<>();
-            while((l=reader.readLine())!=null){
-                lines.add(l);
-            }
-            reader.close();
-
-            for(int i=0;i<lines.size();i++){
-            	String line = lines.get(i);
-            	if(line.startsWith(key)){
-            		lines.set(i, new String(key+"="+value));
-            	}
-            }
-
-            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("src/etfbl/mdp/res/ConnectionProperties.txt")));
-            for(String line : lines){
-            	writer.println(line);
-            }
-            writer.flush();
-            writer.close();
-    	}
-    	catch(IOException e){
-    		e.printStackTrace();
-    	}
-    }
 }
 
